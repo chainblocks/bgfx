1fb7b29538c90c04e2ccf1cd50c3bcccd9174cce
Giovanni Petrantoni <giovanni@fragcolor.xyz>
2021-01-02 23:03:05 +0900

    another fix

--- build-emscripten.sh
+++ build-emscripten.sh
@@ -9,47 +9,47 @@ fi
 
 # regex out of linux config:
 
-sed -i 's/ -mfpmath=sse -msse2//g' bx.make
-sed -i 's/ -mfpmath=sse -msse2//g' fcpp.make
-sed -i 's/ -mfpmath=sse -msse2//g' glslang.make
-sed -i 's/ -mfpmath=sse -msse2/ -fno-strict-aliasing/g' glsl-optimizer.make
-sed -i 's/ -mfpmath=sse -msse2//g' spirv-opt.make
-sed -i 's/ -mfpmath=sse -msse2//g' spirv-cross.make
-sed -i 's/ -mfpmath=sse -msse2//g' shaderc.make
+# sed -i 's/ -mfpmath=sse -msse2//g' bx.make
+# sed -i 's/ -mfpmath=sse -msse2//g' fcpp.make
+# sed -i 's/ -mfpmath=sse -msse2//g' glslang.make
+# sed -i 's/ -mfpmath=sse -msse2/ -fno-strict-aliasing/g' glsl-optimizer.make
+# sed -i 's/ -mfpmath=sse -msse2//g' spirv-opt.make
+# sed -i 's/ -mfpmath=sse -msse2//g' spirv-cross.make
+# sed -i 's/ -mfpmath=sse -msse2//g' shaderc.make
 
-sed -i 's/ -Wl,--as-needed//g' bx.make
-sed -i 's/ -Wl,--as-needed//g' fcpp.make
-sed -i 's/ -Wl,--as-needed//g' glslang.make
-sed -i 's/ -Wl,--as-needed//g' glsl-optimizer.make
-sed -i 's/ -Wl,--as-needed//g' spirv-opt.make
-sed -i 's/ -Wl,--as-needed//g' spirv-cross.make
-sed -i 's/ -Wl,--as-needed//g' shaderc.make
+# sed -i 's/ -Wl,--as-needed//g' bx.make
+# sed -i 's/ -Wl,--as-needed//g' fcpp.make
+# sed -i 's/ -Wl,--as-needed//g' glslang.make
+# sed -i 's/ -Wl,--as-needed//g' glsl-optimizer.make
+# sed -i 's/ -Wl,--as-needed//g' spirv-opt.make
+# sed -i 's/ -Wl,--as-needed//g' spirv-cross.make
+# sed -i 's/ -Wl,--as-needed//g' shaderc.make
 
-sed -i 's/ -Wlogical-op//g' bx.make
-sed -i 's/ -Wlogical-op//g' fcpp.make
-sed -i 's/ -Wlogical-op//g' glslang.make
-sed -i 's/ -Wlogical-op//g' glsl-optimizer.make
-sed -i 's/ -Wlogical-op//g' spirv-opt.make
-sed -i 's/ -Wlogical-op//g' spirv-cross.make
-sed -i 's/ -Wlogical-op//g' shaderc.make
+# sed -i 's/ -Wlogical-op//g' bx.make
+# sed -i 's/ -Wlogical-op//g' fcpp.make
+# sed -i 's/ -Wlogical-op//g' glslang.make
+# sed -i 's/ -Wlogical-op//g' glsl-optimizer.make
+# sed -i 's/ -Wlogical-op//g' spirv-opt.make
+# sed -i 's/ -Wlogical-op//g' spirv-cross.make
+# sed -i 's/ -Wlogical-op//g' shaderc.make
 
-if uname | grep -qiE "mingw|msys"; then
-  sed -i 's/shadercRelease.exe/shaderc.js/g' shaderc.make
-  sed -i 's/shadercDebug.exe/shaderc.js/g' shaderc.make
-else
-  sed -i 's/shadercRelease/shaderc.js/g' shaderc.make
-  sed -i 's/shadercDebug/shaderc.js/g' shaderc.make
-fi
+# if uname | grep -qiE "mingw|msys"; then
+#   sed -i 's/shadercRelease.exe/shaderc.js/g' shaderc.make
+#   sed -i 's/shadercDebug.exe/shaderc.js/g' shaderc.make
+# else
+#   sed -i 's/shadercRelease/shaderc.js/g' shaderc.make
+#   sed -i 's/shadercDebug/shaderc.js/g' shaderc.make
+# fi
 
-sed -i 's/$(SILENT) strip -s "$(TARGET)"//g' shaderc.make
+# sed -i 's/$(SILENT) strip -s "$(TARGET)"//g' shaderc.make
 
-sed -i 's/-O3/-Os/g' bx.make
-sed -i 's/-O3/-Os/g' fcpp.make
-sed -i 's/-O3/-Os/g' glslang.make
-sed -i 's/-O2/-Os/g' glsl-optimizer.make
-sed -i 's/-O3/-Os/g' spirv-opt.make
-sed -i 's/-O3/-Os/g' spirv-cross.make
-sed -i 's/-O3/-Os/g' shaderc.make
+# sed -i 's/-O3/-Os/g' bx.make
+# sed -i 's/-O3/-Os/g' fcpp.make
+# sed -i 's/-O3/-Os/g' glslang.make
+# sed -i 's/-O2/-Os/g' glsl-optimizer.make
+# sed -i 's/-O3/-Os/g' spirv-opt.make
+# sed -i 's/-O3/-Os/g' spirv-cross.make
+# sed -i 's/-O3/-Os/g' shaderc.make
 
 # BUILD_TYPE=debug32
 BUILD_TYPE=release32
@@ -65,7 +65,7 @@ if [ $BUILD_TYPE = "debug32" ]; then
   STRIP_LEVEL=
 fi
 
-ARCH="-s MODULARIZE=1 -s 'EXPORT_NAME="shaderc"' -s 'EXTRA_EXPORTED_RUNTIME_METHODS=["FS"]' -s ALLOW_MEMORY_GROWTH=1 $USE_LTO"
+ARCH="-s MODULARIZE=1 -s 'EXPORT_NAME="shaderc"' -s 'EXTRA_EXPORTED_RUNTIME_METHODS=["FS", "callMain"]' -s ALLOW_MEMORY_GROWTH=1 $USE_LTO"
 DEFINES="-DYYDEBUG=0 $OPT_LEVEL"
 JOBS=`nproc --all`
 
